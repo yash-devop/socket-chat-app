b64a655853952bb71470f6189d57d723832f75fd
Zeid Kazi <zeidkazi7860@gmail.com>
2024-03-28 22:26:06 +0530

    Add: create Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
-import { useMemo } from "react"
-import { io } from "socket.io-client"
+import { useMemo } from "react";
+import { io } from "socket.io-client";
+import Layout from "./layout";
+import { Route, Routes } from "react-router-dom";
+import RoomCredentials from "./pages/RoomCredentials";
 export default function App() {
-  
-  const socket = useMemo(()=>{
-      return io("http://localhost:8000/",{
-          // withCredentials: true
-      })
-  },[])
+  const socket = useMemo(() => {
+    return io("http://localhost:8000/", {
+      // withCredentials: true
+    });
+  }, []);
   return (
-    <div className="">
-      
-    </div>
-  )
+    <Layout>
+      <Routes>
+        <Route  path="/" element={<RoomCredentials />} />
+      </Routes>
+    </Layout>
+  );
 }
