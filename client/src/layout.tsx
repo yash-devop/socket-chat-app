b10ec60a22b09c7ff95d17f30011cdbf5995ec4c
Zeid Kazi <zeidkazi7860@gmail.com>
2024-03-28 22:26:40 +0530

    Add: Layout.tsx

---
+++ client/src/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="w-full max-h-screen h-screen overflow-auto flex flex-col md:flex-row">
+      <div className="w-full min-h-20 md:h-full md:max-w-xl bg-gradient-to-b from-violet-600 via-purple-400 to-pink-500" />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
