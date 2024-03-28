15e72c55b40d907a42e54229831ec883e1b8874a
Zeid Kazi <zeidkazi7860@gmail.com>
2024-03-28 22:25:42 +0530

    Add: Starter

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          avatar: "#D9D9D9",
+          blue: "#3491FF",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
