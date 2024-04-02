02eb6cc730a9489f4e9f1ad64c0242d5bd54f5de
Zeid Kazi <zeidkazi7860@gmail.com>
2024-04-02 21:12:38 +0530

    add: room route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 import Layout from "./layout";
 import { Route, Routes } from "react-router-dom";
 import RoomCredentials from "./pages/RoomCredentials";
+import Room from "./pages/Room";
 export default function App() {
   const socket = useMemo(() => {
     return io("http://localhost:8000/", {
@@ -12,7 +13,8 @@ export default function App() {
   return (
     <Layout>
       <Routes>
-        <Route  path="/" element={<RoomCredentials />} />
+        <Route path="/" element={<RoomCredentials />} />
+        <Route path="/room/:id" element={<Room />} />
       </Routes>
     </Layout>
   );
